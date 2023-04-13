47e8a0806358b9590c9a1ae8fb4b6663f26c8852
Marian <neboduus@gmail.com>
2023-04-13 21:56:18 +0200

    Implement step_2

--- chat_parser/chat_parser.py
+++ chat_parser/chat_parser.py
@@ -1,6 +1,7 @@
 from typing import List
 
-MENTION_SPLIT_CHARACTER = ' : '
+MENTION_DELIMITER = ' : '
+CHAT_LINE_DELIMITER = '\n'
 DATE_MAX_INDEX = 8
 CUSTOMER_TYPE = 'customer'
 AGENT_TYPE = 'agent'
@@ -16,9 +17,9 @@ def get_type(mention: str) -> str:
 
 def parse_line(line: str) -> dict:
     date = line[:DATE_MAX_INDEX]
-    mention = f'{line.split(MENTION_SPLIT_CHARACTER)[0]}' \
-              f'{MENTION_SPLIT_CHARACTER}'
-    sentence = line.split(MENTION_SPLIT_CHARACTER)[1]
+    mention = f'{line.split(MENTION_DELIMITER)[0]}' \
+              f'{MENTION_DELIMITER}'
+    sentence = line.split(MENTION_DELIMITER)[1]
     type = get_type(mention)
     return {
         'date': date,
@@ -28,9 +29,18 @@ def parse_line(line: str) -> dict:
     }
 
 
+def split_chat(chat: str) -> List[str]:
+    lines = chat.split(CHAT_LINE_DELIMITER)
+    return [
+        f'{line}{CHAT_LINE_DELIMITER}'
+        if idx != len(lines) - 1 else line
+        for idx, line in enumerate(lines)
+    ]
+
+
 class ChatParser:
 
     @classmethod
     def parse_chat(cls, chat: str) -> List[dict]:
-        return [parse_line(chat)]
-
+        chat_lines = split_chat(chat)
+        return [parse_line(line) for line in chat_lines]
