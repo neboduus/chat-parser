62c830bbc26572ba7b810fa288c78b2b95cf469e
Marian <neboduus@gmail.com>
2023-04-14 21:13:04 +0200

    Add step_7 test

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -26,6 +26,9 @@ class TestClient(unittest.TestCase):
     def test_step_6(self):
         self.assert_test_case('step_6')
 
+    def test_step_7(self):
+        self.assert_test_case('step_7')
+
     def assert_test_case(self, test_step):
         test_case = get_test_case(test_step)
         output = self.parser.parse_chat(test_case['input'])
